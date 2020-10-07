2f6e0451c7519b3a0839e63dbb159d4a2a8a728f
chuckiex3 <francenemooi@gmail.com>
2020-10-07 21:38:33 +0800

    Implement code for adding tasks

--- src/main/java/seedu/duke/userinterface/CliMessages.java
+++ src/main/java/seedu/duke/userinterface/CliMessages.java
@@ -1,4 +1,7 @@
 package seedu.duke.userinterface;
 
 public class CliMessages {
+    public static void printAddedTaskMessage(String title) {
+        System.out.println("Added task:" + title);
+    }
 }
--- src/main/java/seedu/duke/userinterface/command/Add.java
+++ src/main/java/seedu/duke/userinterface/command/Add.java
@@ -1,8 +1,33 @@
 package seedu.duke.userinterface.command;
 
+import seedu.duke.exceptions.TaskTitleException;
+import seedu.duke.exceptions.TaskWrongFormatException;
+import seedu.duke.tasks.Task;
+import seedu.duke.tasks.TaskList;
+import seedu.duke.userinterface.CliMessages;
+import seedu.duke.userinterface.InputParser;
+
 public class Add extends CliCommand {
+    private final TaskList taskArrayList;
+    private String input;
+
+    public Add (TaskList taskArrayList, String input) {
+        this.taskArrayList = taskArrayList;
+        this.input = input;
+    }
+
     @Override
     public void execute() {
-
+        String title = null;
+        try {
+            title = InputParser.getTaskTitle(input);
+        } catch (TaskTitleException t) {
+            System.out.println("\tYour task is missing a title!");
+            System.out.println("\tPlease type in the format: add /tTITLE");
+        } catch (TaskWrongFormatException w) {
+            System.out.println("\tPlease type in the format: add /tTITLE");
+        }
+        taskArrayList.addTask(new Task(title));
+        CliMessages.printAddedTaskMessage(title);
     }
 }
