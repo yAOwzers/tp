6d5fcb4170786ac58f43b17a371eb869f7c55aec
neilbaner <30567828+NeilBaner@users.noreply.github.com>
2020-10-11 18:25:00 +0800

    Add some code for the help command

--- src/main/java/seedu/duke/userinterface/CliMessages.java
+++ src/main/java/seedu/duke/userinterface/CliMessages.java
@@ -1,4 +1,62 @@
 package seedu.duke.userinterface;
 
 public class CliMessages {
+
+    public void printAddTaskHelp() {
+
+    }
+    public void printAddNotebookSectionPageHelp() {
+
+    }
+    public void printDoneTaskHelp() {
+
+    }
+    public void printExitHelp() {
+
+    }
+    public void printListTaskHelp() {
+
+    }
+    public void printListNotebookSectionPageHelp() {
+
+    }
+    public void printModeSwitchHelp() {
+
+    }
+    public void printRemoveTaskHelp() {
+
+    }
+    public void printRemoveNotebookSectionPageHelp() {
+
+    }
+    public void printSelectHelp() {
+
+    }
+
+    public void printNotebookModeHelp() {
+        System.out.println("Here are some commands to help you work with the Notebook mode: ");
+        printAddNotebookSectionPageHelp();
+        printListNotebookSectionPageHelp();
+        printSelectHelp();
+        printRemoveNotebookSectionPageHelp();
+    }
+    public void printTimetableModeHelp() {
+        System.out.println("Here are some commands to help you work with the Timetable mode: ");
+        printAddTaskHelp();
+        printDoneTaskHelp();
+        printListTaskHelp();
+        printRemoveTaskHelp();
+    }
+    public void printGeneralHelp() {
+        System.out.println("Here are some general commands that will work throughout Zer0Note: ");
+        printModeSwitchHelp();
+        printExitHelp();
+    }
+
+    public void printAllHelp() {
+        System.out.println("Here are all the commands you need to know to operate Zer0Note: ");
+        printGeneralHelp();
+        printNotebookModeHelp();
+        printTimetableModeHelp();
+    }
 }
--- src/main/java/seedu/duke/userinterface/command/Help.java
+++ src/main/java/seedu/duke/userinterface/command/Help.java
@@ -1,8 +1,23 @@
 package seedu.duke.userinterface.command;
 
+import seedu.duke.userinterface.CliMessages;
+
 public class Help extends CliCommand {
+    String commandArgs;
+    public Help(String commandArgs) {
+        this.commandArgs = commandArgs;
+    }
     @Override
     public void execute() {
-
+        CliMessages messages = new CliMessages();
+        messages.printGeneralHelp();
+        switch(commandArgs) {
+        case "notebook":
+            messages.printNotebookModeHelp();
+            break;
+        case "timetable":
+            messages.printTimetableModeHelp();
+            break;
+        }
     }
 }
