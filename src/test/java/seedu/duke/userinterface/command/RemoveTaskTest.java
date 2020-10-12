a512458e3726d117747c278f2cfdc5ff2621feac
Lusi711 <e0424985@u.nus.edu>
2020-10-12 23:23:28 +0800

    Implement JUnit tests for RemoveTask() method

---
+++ src/test/java/seedu/duke/userinterface/command/RemoveTaskTest.java
@@ -0,0 +1,40 @@
+package seedu.duke.userinterface.command;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import seedu.duke.tasks.TaskList;
+import seedu.duke.userinterface.AppState;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class RemoveTaskTest {
+    @Test
+    void execute_DeleteTaskOne_NoTasksInTaskList() {
+        TaskList taskslist = new TaskList();
+        AppState appState = new AppState();
+
+        String input = "add /tTask1 /by12-10-2020 2252";
+        Add add = new Add(input,appState);
+        add.execute();
+
+        RemoveTask remove = new RemoveTask(1,appState);
+        remove.execute();
+
+        assertEquals(taskslist.getNumberOfTasks(), 0);
+    }
+
+    @Test
+    void execute_DeleteTaskZero_IndexOutOfBoundsExceptionThrown() {
+        TaskList taskslist = new TaskList();
+        AppState appState = new AppState();
+
+        String input = "add /tTask1 /by12-10-2020 2252";
+        Add add = new Add(input,appState);
+        add.execute();
+
+        RemoveTask remove = new RemoveTask(-1,appState);
+        Assertions.assertThrows(IndexOutOfBoundsException.class, () -> {
+            remove.execute();
+        });
+    }
+}
