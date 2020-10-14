13fac137d9b2d59f742b452c727f264c30d8aaad
neilbaner <30567828+NeilBaner@users.noreply.github.com>
2020-10-14 18:44:52 +0800

    Formatting

--- src/main/java/seedu/duke/exceptions/ZeroNoteException.java
+++ src/main/java/seedu/duke/exceptions/ZeroNoteException.java
@@ -1,12 +1,15 @@
 package seedu.duke.exceptions;
 
-public abstract class ZeroNoteException extends Exception{
+public abstract class ZeroNoteException extends Exception {
     public String problematicInput;
+
     public ZeroNoteException() {
 
     }
+
     public ZeroNoteException(String enteredCommand) {
         this.problematicInput = enteredCommand;
     }
+
     public abstract void printErrorMessage();
 }
