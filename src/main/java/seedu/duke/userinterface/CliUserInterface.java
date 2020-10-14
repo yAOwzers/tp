65d1874bf09ced87d86117ccb7b98f86951cad17
neilbaner <30567828+NeilBaner@users.noreply.github.com>
2020-10-14 19:10:22 +0800

    Add TODOs

--- src/main/java/seedu/duke/userinterface/CliUserInterface.java
+++ src/main/java/seedu/duke/userinterface/CliUserInterface.java
@@ -1,5 +1,6 @@
 package seedu.duke.userinterface;
 
+import seedu.duke.exceptions.ZeroNoteException;
 import seedu.duke.storage.Storage;
 import seedu.duke.userinterface.command.CliCommand;
 import seedu.duke.userinterface.command.Exit;
@@ -38,8 +39,11 @@ public class CliUserInterface {
             }
             try {
                 executeCommand(userInput);
+            } catch (ZeroNoteException e) {
+                e.printErrorMessage();
             } catch (Exception e) {
-                // TODO: actually make exceptions that make sense
+                // TODO: Make sure all Java exceptions are handled and converted to ZeroNoteExceptions
+                // TODO: Delete this block after the above is done
                 e.printStackTrace();
             }
         }
