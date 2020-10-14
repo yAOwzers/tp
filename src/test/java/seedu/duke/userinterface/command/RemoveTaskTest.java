aeac9427ad8735c49118e72252714e7261b15229
neilbaner <30567828+NeilBaner@users.noreply.github.com>
2020-10-14 19:10:13 +0800

    Refactor

--- src/test/java/seedu/duke/userinterface/command/RemoveTaskTest.java
+++ src/test/java/seedu/duke/userinterface/command/RemoveTaskTest.java
@@ -3,8 +3,8 @@ package seedu.duke.userinterface.command;
 import org.junit.jupiter.api.Test;
 import seedu.duke.tasks.TaskList;
 import seedu.duke.userinterface.AppState;
-import seedu.duke.userinterface.command.timetable.AddTimetableMode;
-import seedu.duke.userinterface.command.timetable.RemoveTimetableMode;
+import seedu.duke.userinterface.command.timetable.AddCommandTimetableMode;
+import seedu.duke.userinterface.command.timetable.RemoveCommandTimetableMode;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -15,10 +15,10 @@ class RemoveTaskTest {
         AppState appState = new AppState();
 
         String input = "add /tTask1 /by12-10-2020 2252";
-        AddTimetableMode addTimetableMode = new AddTimetableMode(input,appState);
+        AddCommandTimetableMode addTimetableMode = new AddCommandTimetableMode(input,appState);
         addTimetableMode.execute();
 
-        RemoveTimetableMode removeTimetableMode = new RemoveTimetableMode(1,appState);
+        RemoveCommandTimetableMode removeTimetableMode = new RemoveCommandTimetableMode(1,appState);
         removeTimetableMode.execute();
 
         assertEquals(taskslist.getNumberOfTasks(), 0);
