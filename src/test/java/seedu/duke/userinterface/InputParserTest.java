38ee25102859cbef8266e016d4d98bde3cfc2129
neilbaner <30567828+NeilBaner@users.noreply.github.com>
2020-10-25 16:14:58 +0800

    Add more tests for InputParser
    Some of them fail at the moment, this needs to be fixed

--- src/test/java/seedu/duke/userinterface/InputParserTest.java
+++ src/test/java/seedu/duke/userinterface/InputParserTest.java
@@ -2,8 +2,16 @@ package seedu.duke.userinterface;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import seedu.duke.exceptions.IncorrectDeadlineFormatException;
+import seedu.duke.exceptions.TaskWrongFormatException;
+import seedu.duke.exceptions.ZeroNoteException;
 
+import java.util.concurrent.atomic.AtomicReference;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
 
 class InputParserTest {
     @Test
@@ -20,5 +28,79 @@ class InputParserTest {
         Assertions.assertThrows(NumberFormatException.class, () -> {
             parser.parseTaskIndex(num);
         });
+    }
+
+    @Test
+    void parseTaskTitle_correctFormat() {
+        InputParser parser = new InputParser();
+        String input = "/tBlowing Bubbles /by2020-10-10";
+        try {
+            assertEquals("Blowing Bubbles", parser.parseTaskTitle(input));
+        } catch (Exception e) {
+            Assertions.fail();
+        }
+    }
+
+    @Test
+    void parseTaskTitle_withSpaces() {
+        InputParser parser = new InputParser();
+        String input = "/t Blowing Bubbles /by 2020-10-10";
+        try {
+            assertEquals("Blowing Bubbles", parser.parseTaskTitle(input));
+        } catch (Exception e) {
+            Assertions.fail();
+        }
+    }
+
+    @Test
+    void parseTaskTitle_noDate() {
+        InputParser parser = new InputParser();
+        String input = "/t Blowing Bubbles";
+        assertThrows(ZeroNoteException.class, () -> {
+            String output = parser.parseTaskTitle(input);
+        });
+    }
+
+    @Test
+    void parseDeadline_validFormat() {
+        InputParser parser = new InputParser();
+        String input = "/tBlowing Bubbles /by10-10-2020 1000";
+        try {
+            assertEquals("10-10-2020 1000", parser.parseDeadline(input));
+        } catch (Exception e) {
+            Assertions.fail();
+        }
+    }
+
+    @Test
+    void parseDeadline_validFormatWithSpaces() {
+        InputParser parser = new InputParser();
+        String input = "/t Blowing Bubbles /by 10-10-2020 1000";
+        try {
+            assertEquals("10-10-2020 1000", parser.parseDeadline(input));
+        } catch (Exception e) {
+            Assertions.fail();
+        }
+    }
+
+    @Test
+    void parseDeadline_invalidFormat() {
+        InputParser parser = new InputParser();
+        String input = "/tBlowing Bubbles /by 2020-10-10 12";
+        assertThrows(IncorrectDeadlineFormatException.class, () -> parser.parseDeadline(input));
+    }
+
+    @Test
+    void parseDeadline_noDate() {
+        InputParser parser = new InputParser();
+        String input = "/tBlowing Bubbles";
+        assertThrows(IncorrectDeadlineFormatException.class, () -> parser.parseDeadline(input));
+    }
+
+    @Test
+    void parseDeadline_blankDate() {
+        InputParser parser = new InputParser();
+        String input = "/tBlowing Bubbles /by";
+        assertThrows(IncorrectDeadlineFormatException.class, () -> parser.parseDeadline(input));
     }
 }
