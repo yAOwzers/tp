35a8079bf42bc1b4f1b516e72f8115c8a9b94ef8
Lusi711 <e0424985@u.nus.edu>
2020-11-03 09:34:40 +0800

    Add JUnit Test for Find

---
+++ src/main/java/seedu/duke/exceptions/InvalidKeywordTagException.java
@@ -0,0 +1,14 @@
+package seedu.duke.exceptions;
+
+public class InvalidKeywordTagException extends ZeroNoteException{
+    public InvalidKeywordTagException(String problematicInput) {
+        super(problematicInput);
+    }
+
+    @Override
+    public void printErrorMessage() {
+        System.out.println("Invalid/Missing keyword or tag: " + problematicInput);
+        System.out.println("Format for finding by keyword: find KEYWORD");
+        System.out.println("Format for finding by tag: find /tTAG");
+    }
+}
