5b286383b43fcc02d73a716ec6dc44403dead657
Lusi711 <e0424985@u.nus.edu>
2020-11-05 18:53:10 +0800

    Add JUnit test for tag task

---
+++ src/test/java/seedu/duke/userinterface/command/timetable/TagTaskTest.java
@@ -0,0 +1,44 @@
+package seedu.duke.userinterface.command.timetable;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.duke.tasks.Task;
+import seedu.duke.userinterface.AppState;
+import seedu.duke.userinterface.command.notebook.TagCommandNotebookMode;
+
+class TagTaskTest {
+
+    @Test
+    void execute_tagExistingTask_tagSet() {
+        AppState appState = new AppState();
+        Task task = new Task("Task 1","05-11-2020 1847");
+        appState.getTaskList().addTask(task);
+
+        TagCommandTimetableMode t = new TagCommandTimetableMode(0,"test",appState);
+        t.execute();
+        task = appState.getTaskList().getTask(0);
+        assertEquals("test",task.getTag());
+    }
+
+    @Test
+    void execute_invalidIndex_noTagSet() {
+        AppState appState = new AppState();
+        Task task = new Task("Task 1","05-11-2020 1847");
+        appState.getTaskList().addTask(task);
+
+        TagCommandTimetableMode t = new TagCommandTimetableMode(-1,"test",appState);
+        t.execute();
+        task = appState.getTaskList().getTask(0);
+        assertEquals("",task.getTag());
+    }
+
+    @Test
+    void isTriggerAutoSave() {
+        AppState appState = new AppState();
+        TagCommandNotebookMode t = new TagCommandNotebookMode("", appState);
+        assertTrue(t.isTriggerAutoSave());
+    }
+}
