8ba5d4d6cb884257976b123e19eef54adc067f5e
chuckiex3 <francenemooi@gmail.com>
2020-11-06 14:36:57 +0800

    Add new test

--- src/test/java/zeronote/userinterface/command/notebook/AddNotebookTest.java
+++ src/test/java/zeronote/userinterface/command/notebook/AddNotebookTest.java
@@ -2,6 +2,7 @@ package zeronote.userinterface.command.notebook;
 
 import org.junit.jupiter.api.Test;
 import zeronote.exceptions.InvalidCommandException;
+import zeronote.exceptions.ZeroNoteException;
 import zeronote.userinterface.AppMode;
 import zeronote.userinterface.AppState;
 import zeronote.userinterface.InputParser;
@@ -30,5 +31,17 @@ public class AddNotebookTest {
         assertThrows(InvalidCommandException.class, () -> {
             parser.getCommandFromInput(inputString, appState);
         });
+    }
+
+    @Test
+    void addNotebook_sectionPage_expectException() {
+        InputParser parser = new InputParser();
+        AppState appState = new AppState();
+        appState.setAppMode(AppMode.NOTEBOOK_SHELF);
+        String userInput = "add /nNotebook /sSection /pPage";
+        assertThrows(ZeroNoteException.class, () -> {
+           parser.getCommandFromInput(userInput, appState);
+        });
+
     }
 }
